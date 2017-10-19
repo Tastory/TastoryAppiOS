93ece816788afc04556407372b666aae050bd2bc
Howard Lee <howard.lee@tastry.co>
2017-10-19 15:05:43 -0700

    Move to Master S3/Parse/Bundle

--- TastryApp/FoodiePFObject.swift
+++ TastryApp/FoodiePFObject.swift
@@ -29,8 +29,8 @@ class FoodiePFObject: PFObject {
     Parse.enableLocalDatastore()
     
     let configuration = ParseClientConfiguration {
-      $0.applicationId = "XWWoiDtH0kOo2Y5w9QIHq5TecpOBj6CwaRiArb7o"
-      $0.clientKey = "f3j1ztnsbi8sBKQJKKK0c8pPmd90bPLWw75WwNKW"
+      $0.applicationId = "HTMKtzMDQsuWXp9g0te0uYg5JQGuptZOQmmR46BK"
+      $0.clientKey = "oX4mM0xixVcdmQANyGXmgqxlo0VTYld1dg1dO8P1"
       $0.server = "https://parseapi.back4app.com"
       $0.isLocalDatastoreEnabled = true
     }
