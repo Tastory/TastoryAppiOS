15c3d4544d60fb726c2f157e4d96c1dbb7dca4b2
Howard Lee <howard.lee@tastry.co>
2017-10-19 17:07:53 -0700

    Updated Pods and Reconstructed xcworkspace

--- Pods/TLPhotoPicker/TLPhotoPicker/Classes/TLPhotosPickerViewController.swift
+++ Pods/TLPhotoPicker/TLPhotoPicker/Classes/TLPhotosPickerViewController.swift
@@ -34,6 +34,8 @@ public struct TLPhotosPickerConfigure {
     public var usedPrefetch = false
     public var allowedLivePhotos = true
     public var allowedVideo = true
+    public var autoPlay = true
+    public var muteAudio = false
     public var mediaType: PHAssetMediaType? = nil
     public var numberOfColumn = 3
     public var maxSelectedAssets: Int? = nil
@@ -71,7 +73,8 @@ open class TLPhotosPickerViewController: UIViewController {
     @IBOutlet open var customNavItem: UINavigationItem!
     @IBOutlet open var doneButton: UIBarButtonItem!
     @IBOutlet open var cancelButton: UIBarButtonItem!
-
+    @IBOutlet open var navigationBarTopConstraint: NSLayoutConstraint!
+    
     public weak var delegate: TLPhotosPickerViewControllerDelegate? = nil
     public var selectedAssets = [TLPHAsset]()
     public var configure = TLPhotosPickerConfigure()
@@ -167,6 +170,10 @@ open class TLPhotosPickerViewController: UIViewController {
         super.viewDidLayoutSubviews()
         if self.thumbnailSize == CGSize.zero {
             initItemSize()
+        }
+        if #available(iOS 11.0, *) {
+        } else if self.navigationBarTopConstraint.constant == 0 {
+            self.navigationBarTopConstraint.constant = 20
         }
     }
     
@@ -465,7 +472,7 @@ extension TLPhotosPickerViewController {
                 playVideo(asset: asset.1, indexPath: asset.0)
             }
         }
-        guard self.allowedVideo || self.allowedLivePhotos else { return }
+        guard self.configure.autoPlay else { return }
         guard self.playRequestId == nil else { return }
         let visibleIndexPaths = self.collectionView.indexPathsForVisibleItems.sorted(by: { $0.row < $1.row })
         let boundAssets = visibleIndexPaths.flatMap{ indexPath -> (IndexPath,TLPHAsset)? in
@@ -490,23 +497,23 @@ extension TLPhotosPickerViewController: PHLivePhotoViewDelegate {
     }
     
     fileprivate func playVideo(asset: TLPHAsset, indexPath: IndexPath) {
-      stopPlay()
-        guard self.allowedVideo || self.allowedLivePhotos else { return }
+        stopPlay()
         guard let phAsset = asset.phAsset else { return }
         if asset.type == .video {
             guard let cell = self.collectionView.cellForItem(at: indexPath) as? TLPhotoCollectionViewCell else { return }
             let requestId = self.photoLibrary.videoAsset(asset: phAsset, completionBlock: { (playerItem, info) in
-                DispatchQueue.main.sync { [weak cell] in
-                    guard let cell = cell, cell.player == nil else { return }
+                DispatchQueue.main.sync { [weak self, weak cell] in
+                    guard let `self` = self, let cell = cell, cell.player == nil else { return }
                     let player = AVPlayer(playerItem: playerItem)
                     cell.player = player
                     player.play()
+                    player.isMuted = self.configure.muteAudio
                 }
             })
             if requestId > 0 {
                 self.playRequestId = (indexPath,requestId)
             }
-        }else if asset.type == .livePhoto, self.allowedLivePhotos {
+        }else if asset.type == .livePhoto {
             guard let cell = self.collectionView.cellForItem(at: indexPath) as? TLPhotoCollectionViewCell else { return }
             let requestId = self.photoLibrary.livePhotoAsset(asset: phAsset, size: self.thumbnailSize, completionBlock: { (livePhoto) in
                 cell.livePhotoView?.isHidden = false
@@ -615,7 +622,7 @@ extension TLPhotosPickerViewController: UICollectionViewDelegate,UICollectionVie
             //requestCloudDownload(asset: asset, indexPath: indexPath)
             cell.selectedAsset = true
             cell.orderLabel?.text = "\(asset.selectedOrder)"
-            if asset.type != .photo {
+            if asset.type != .photo, self.configure.autoPlay {
                 playVideo(asset: asset, indexPath: indexPath)
             }
         }
