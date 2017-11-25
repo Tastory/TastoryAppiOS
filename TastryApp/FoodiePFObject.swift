dd4c6902b343d62f8a52956c487b646b9c33ffd9
Howard Lee <howard.lee@tastry.co>
2017-11-24 21:45:22 -0800

    Move back to Main Parse/S3/Bundle
    This reverts commit 81f494e8cc99b389b87032cb1cab2bb4e8eb53e1.

--- TastryApp/FoodiePFObject.swift
+++ TastryApp/FoodiePFObject.swift
@@ -20,7 +20,6 @@ class FoodiePFObject: PFObject {
   }
   
   
-  
   // MARK: - Public Instance Variables
   var foodieObject: FoodieObject!
   
@@ -31,8 +30,8 @@ class FoodiePFObject: PFObject {
     Parse.enableLocalDatastore()
     
     let configuration = ParseClientConfiguration {
-      $0.applicationId = "62D1obZSFbDyavis57XLnIZyjyh8E5MuixrTodIn"
-      $0.clientKey = "l5xadWiP1pp6P8K1MmlF4zfs35uW3WBURPrFidBI"
+      $0.applicationId = "HTMKtzMDQsuWXp9g0te0uYg5JQGuptZOQmmR46BK"
+      $0.clientKey = "oX4mM0xixVcdmQANyGXmgqxlo0VTYld1dg1dO8P1"
       $0.server = "https://parseapi.back4app.com"
       $0.isLocalDatastoreEnabled = true
     }
